7a2895eafc308c42deeefb4a517fa557adc5bc7a
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2023-05-20 10:09:01 +0700

    update plugin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,28 +39,32 @@ export default class MyDocument extends Document {
 
         <div id="fb-customer-chat" className="fb-customerchat"></div>
 
-        <Script strategy="lazyOnload" id="facebook-chat">
-          {`
-            var chatbox = document.getElementById('fb-customer-chat');
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var chatbox = document.getElementById("fb-customer-chat");
             chatbox.setAttribute("page_id", "119952837764885");
             chatbox.setAttribute("attribution", "biz_inbox");
-      
-            window.fbAsyncInit = function() {
+
+            window.fbAsyncInit = function () {
               FB.init({
-                xfbml            : true,
-                version          : 'v16.0'
+                xfbml: true,
+                version: "v16.0",
               });
             };
 
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
+            (function (d, s, id) {
+              var js,
+                fjs = d.getElementsByTagName(s)[0];
               if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
               fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-          `}
-        </Script>
+            })(document, "script", "facebook-jssdk");
+          `,
+          }}
+        />
       </Html>
     )
   }
