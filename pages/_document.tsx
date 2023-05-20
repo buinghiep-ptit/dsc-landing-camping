4053b80bc362a68e12a345598e86a7d0138d76a2
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2023-05-20 09:58:18 +0700

    fix: update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/inline-script-id */
 import * as React from "react"
 import Document, { Html, Head, Main, NextScript } from "next/document"
 import createEmotionServer from "@emotion/server/create-instance"
 import { theme, createEmotionCache } from "@/utils"
+import Script from "next/script"
 
 export default class MyDocument extends Document {
   render() {
@@ -33,6 +35,36 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <div id="fb-root"></div>
+
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var chatbox = document.getElementById("fb-customer-chat");
+            chatbox.setAttribute("page_id", "119952837764885");
+            chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function () {
+              FB.init({
+                xfbml: true,
+                version: "v16.0",
+              });
+            };
+
+            (function (d, s, id) {
+              var js,
+                fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            })(document, "script", "facebook-jssdk");
+          `,
+          }}
+        />
       </Html>
     )
   }
