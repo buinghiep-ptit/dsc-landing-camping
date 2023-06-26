5237bf0cded57db82647fd51b1f18254d6a16859
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2023-06-26 22:28:30 +0700

    fix: fix update rewrites api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://cahn-api.campdi.vn/cahnfc/api/:path*", // Proxy to Backend
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
