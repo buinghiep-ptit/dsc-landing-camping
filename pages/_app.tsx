55b97a659081551d71d945c6757c039467593b71
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2023-08-09 11:03:55 +0700

    fix: update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,28 @@ function App({
 }: AppProps) {
   const Layout = Component.Layout ?? EmptyLayout
 
+  const fbChatContent = `
+            var chatbox = document.getElementById("fb-customer-chat");
+            chatbox.setAttribute("page_id", "119952837764885");
+            chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function () {
+              FB.init({
+                xfbml: true,
+                version: "v16.0",
+              });
+            };
+
+            (function (d, s, id) {
+              var js,
+                fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            })(document, "script", "facebook-jssdk");
+          `
   return (
     <>
       <CacheProvider value={emotionCache}>
@@ -34,6 +56,9 @@ function App({
           </ThemeProvider>
         </StyledEngineProvider>
       </CacheProvider>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <script dangerouslySetInnerHTML={{ __html: fbChatContent }} />
     </>
   )
 }
